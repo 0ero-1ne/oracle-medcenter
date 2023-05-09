85dd5ebc74e6f583d24e4d5c5367b09c411104e4
0ero-1ne <volikovd17@gmail.com>
2023-05-09 23:16:41 +0300

    [App] Yii2 application

---
+++ medcent/config/db.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'oci:dbname=//localhost:1521/ORCL;charset=AL32UTF8',
+    'username' => 'system',
+    'password' => '$Ad0129$',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
